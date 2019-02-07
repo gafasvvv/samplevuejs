244c430d05639f70a6d16cd7702335ca675a77eb
gafasvvv <ry0suke.programming@gmail.com>
2019-02-07 19:56:05 +0900

    samplevuex

---
+++ samplevuex/src/components/Form.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+      お問い合わせ入力欄
+    <HeadComp></HeadComp>
+    <component :is="isComponent"></component>
+    <button v-on:click="buttonAction">{{ button }}</button>
+  </div>
+</template>
+
+<script>
+import HeadComp from '@/components/modules/HeadComp'
+import TextareaComp from '@/components/modules/TextareaComp'
+import StringComp from '@/components/modules/StringComp'
+import { mapActions, mapGetters } from 'vuex'//mapActions:Vuexのアクションを使うためのもの
+
+export default {
+  name: 'form',
+  methods: mapActions('Form', {
+      'buttonAction': 'buttonAction'
+  }),
+  computed: mapGetters('Form', {
+      'button': 'getButton',
+      'isComponent': 'getComponent'
+  }),
+  components: {
+    HeadComp,
+    TextareaComp,
+    StringComp,
+  }
+}
+</script>
---
+++ samplevuex/src/components/Thanks.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <HeadComp></HeadComp>
+        <p>送信ありがとうございました！</p>
+    </div>
+</template>
+
+<script>
+import HeadComp from '@/components/modules/HeadComp'
+import { mapActions, mapGetters } from 'vuex'
+
+export default {
+    name: 'thanks',
+    components: {
+        HeadComp
+    }    
+}
+</script>
+
